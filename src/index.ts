7884fc37a3b6b05725f6aa0e35eebc894ada7154
Giancarmine <giancarmine.cuofano@gmail.com>
2023-05-05 23:16:54 +0000

    fix: structure who command

--- src/index.ts
+++ src/index.ts
@@ -29,15 +29,10 @@ bot.command("start", async (ctx) => {
 });
 
 bot.command("who", async (ctx) => {
-	await ctx.reply(`Your id is: ${ctx.from?.id}`);
-	await ctx.reply(`Your name is: ${ctx.from?.first_name}`);
-
 	const profilesPhotos = await ctx.getUserProfilePhotos(ctx.from.id);
 
 	await ctx.replyWithPhoto(profilesPhotos.photos[0][0].file_id);
-	// profilesPhotos.photos.forEach(photo => {
-	//	ctx.replyWithPhoto(photo[0].file_id);
-	//});
+	await ctx.reply(`Your name is: @${ctx.from?.first_name}`);
 });
 
 bot.command("help", async (ctx) => {

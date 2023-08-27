a5c342eaa705abaa7ff013e2295b5cc5c3e03201
El_Giancar <giancarmine.cuofano@gmail.com>
2023-08-27 15:00:20 +0200

    fixie

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,11 +1,11 @@
 import { Bot } from "grammy/web";
-import process from "node:process";
+// import process from "node:process";
 import { BotContext } from "./context";
 
-const bot_token = process.env["TOKEN"];
+// const bot_token = process.env["TOKEN"];
 
-if (!bot_token) throw new Error("TOKEN is required as an environment variable!");
+// if (!bot_token) throw new Error("TOKEN is required as an environment variable!");
 
-const bot = new Bot<BotContext>(bot_token);
+const bot = new Bot<BotContext>(BOT_TOKEN);
 
 export default bot

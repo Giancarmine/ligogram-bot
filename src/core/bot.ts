6467289df5c81f88674ed5bc9ae3e0aef859ab02
El_Giancar <giancarmine.cuofano@gmail.com>
2023-09-10 20:59:35 +0200

    rollback

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,6 +1,6 @@
 import { Bot } from "grammy/web";
 import { BotContext } from "./context";
 
-const bot = new Bot<BotContext>(process.env.BOT_TOKEN);
+const bot = new Bot<BotContext>(BOT_TOKEN);
 
 export default bot;

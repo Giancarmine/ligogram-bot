39eb8cdf3c1c3c8d6b1f4d52743e149283e31f29
El_Giancar <giancarmine.cuofano@gmail.com>
2023-09-10 20:20:54 +0200

    fix: switch to process to retrive env vars

--- src/core/bot.ts
+++ src/core/bot.ts
@@ -1,7 +1,6 @@
 import { Bot } from "grammy/web";
 import { BotContext } from "./context";
-import env from "./env";
 
-const bot = new Bot<BotContext>(env.BOT_TOKEN);
+const bot = new Bot<BotContext>(process.env.BOT_TOKEN);
 
 export default bot;
